2c1eeb8f215aa86b6d63b577622a53bddc2c65df
magdabot <magdabot@users.noreply.github.com>
2026-07-14 06:52:32 +0000

    deploy: magda-io/magda@bbc50b96d4a5189aa0004d0b375bd1347de0d6b8

--- api_project.js
+++ api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2026-07-14T06:05:21.448Z",
+    "time": "2026-07-14T06:52:30.866Z",
     "url": "https://apidocjs.com",
     "version": "0.17.6"
   }
